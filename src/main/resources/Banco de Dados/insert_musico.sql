fddc49031d4891ee9c6c3a79b50647301ddcff96
Paulo Amorim <paulovitor.10@hotmail.com>
2016-05-15 21:55:28 -0300

    inclusão de arquivos do banco de dados

---
+++ src/main/resources/Banco de Dados/insert_musico.sql
@@ -0,0 +1,3 @@
+insert into musico values(
+'1', 'Paulo', 'Amorim', 'paulovitor.10@hotmail.com', '123456', '1995-09-03', 'M', '3133811593', '31992516635', 11, 1630, 'Barreiro', 'Albertino Teixeira Dias', 'apto 201')
+
